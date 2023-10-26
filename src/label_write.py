430a87b0009f120e5ce6e09d5db96b6060dd9259
Apolline <apolline.de_vaulchier@ensta-bretagne.org>
2023-10-26 10:30:15 +0200

    mise à jour

--- src/label_write.py
+++ src/label_write.py
@@ -9,16 +9,14 @@ for name in dossier:
     contenu = contenu.split()
 
     # Calcul des valeurs centre, width, height
-    center_x = float(contenu[0]) + (float(contenu[2]) - float(contenu[0])) / 2
-    center_y = float(contenu[1]) + (float(contenu[7]) - float(contenu[1])) / 2
-    width = float(contenu[2]) - float(contenu[0])
-    height = float(contenu[7]) - float(contenu[1])
+    center_x = float(contenu[1]) + (float(contenu[3]) - float(contenu[1])) / 2
+    center_y = float(contenu[2]) + (float(contenu[8]) - float(contenu[2])) / 2
+    width = float(contenu[3]) - float(contenu[1])
+    height = float(contenu[8]) - float(contenu[2])
 
     #Ecriture dans le fichier
     new_ligne = "0 " + str(center_x) + " " + str(center_y) + " " + str(width) + " " + str(height)
     fichier = open(name, "w")
-    contenu[1] = str(float(contenu[1]) + 0.5)
-    contenu[2] = str(float(contenu[2]) + 0.5)
     fichier = open("new/" + name, "w")
     fichier.write(new_ligne)
     fichier.close()
